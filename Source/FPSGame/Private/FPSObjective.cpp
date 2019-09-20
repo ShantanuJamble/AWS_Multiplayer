9ff2bb5a685267b620eb7645821e4f621df6fea6
ShantanuJamble <sjamble24@gmail.com>
2019-09-20 19:17:13 -0400

    Started working on the objective class

---
+++ Source/FPSGame/Private/FPSObjective.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSObjective.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AFPSObjective::AFPSObjective()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	SetRootComponent(MeshComp);
+
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSpehere"));
+	CollisionSphere->SetupAttachment(MeshComp);
+
+}
+
+// Called when the game starts or when spawned
+void AFPSObjective::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFPSObjective::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
