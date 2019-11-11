40ad6f01fed1ad004065e58196f63fd3c13189fd
Shantanu Jamble (RIT Student) <sj6045@ad.rit.edu>
2019-11-10 19:30:00 -0500

    Downloading s3 files successfully

--- AWSTools/s3Downloader.py
+++ AWSTools/s3Downloader.py
@@ -7,37 +7,50 @@ import os
 
 class S3Downloader(object):
 
-
     @staticmethod
-    def GetFileList(bucket,folder_path,region= 'us-west-1'):
+    def GetFileList(bucket, folder_path, region='us-west-1'):
         file_list = []
         try:
             s3resource = boto3.resource('s3', region_name=region)
-            s3Bucket = s3resource.Bucketx(bucket)
-            for obj in  s3Bucket.objects.filter(Prefix=folder_path):
+            s3Bucket = s3resource.Bucket(bucket)
+            for obj in s3Bucket.objects.filter(Prefix=folder_path):
                 file_list.append(obj.key)
-            print (file_list)
+            print(file_list)
+            return file_list
         except Exception as e:
             raise Exception(str(e))
 
-
     @staticmethod
-    def Downloadfile(bucket, file_path, region="us-west-1"):
+    def Downloadfile(bucket, folder_path, local_path, region="us-west-1"):
 
-        s33client = boto3.client('s3', region_name=region)
+        s3client = boto3.client('s3', region_name=region)
         s3resource = boto3.resource('s3', region_name=region)
         try:
-            folder = s3resource.Object(bucket, file_path).load()
+
+            files_list = S3Downloader.GetFileList(bucket, folder_path, region)
+            paginator = s3client.get_paginator('list_objects')
+            # folder_data = s3resource.Object(bucket, folder_path).load()
+            for result in paginator.paginate(Bucket=bucket, Delimiter='/', Prefix=file_path):
+                if result.get('CommonPrefixes') is not None:
+                    print(result.get('CommonPrefixes'))
+                    for subdir in result.get('CommonPrefixes'):
+                        S3Downloader.Downloadfile(s3client, s3resource, local_path, subdir.get('Prefix'))
+                if result.get('Contents') is not None:
+                    for file in result.get('Contents'):
+                        if not os.path.exists(os.path.dirname(local_path + os.sep + file.get('Key'))):
+                            os.makedirs(os.path.dirname(local_path + os.sep + file.get('Key')))
+                        print(os.path.dirname(file.get('Key')))
+                        #s3resource.meta.client.download_file(bucket, file.get('Key'),
+                         #                                    local_path + os.sep + file.get('Key'))
+
             print("File found")
-        except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] == "404":
-                exists = False
-            else:
-                raise
+        except Exception as e:
+            print(str(e))
 
 
 if __name__ == "__main__":
     bucket = 'logs-gamelift'
-    #file_path = 'Gamelift/AWSLogs/136816550245/CloudTrail/us-east-1/2019/11/09/136816550245_CloudTrail_us-east-1_20191109T2050Z_3LClu2QyVluXwhlq.json.gz'
+    # file_path = 'Gamelift/AWSLogs/136816550245/CloudTrail/us-east-1/2019/11/09/136816550245_CloudTrail_us-east-1_20191109T2050Z_3LClu2QyVluXwhlq.json.gz'
     file_path = 'Gamelift/AWSLogs/136816550245/CloudTrail/us-east-1/2019/11/09/'
-    test = S3Downloader.GetFileList(bucket=bucket, folder_path=file_path)
+    local_path = "D:/PycharmProjects/GameLift_Monitor/downloads"
+    S3Downloader.Downloadfile(bucket=bucket, folder_path=file_path, local_path=local_path)
--- Research_docs/Resources.md
+++ Research_docs/Resources.md
@@ -1,5 +1,5 @@
-## Matchmaking tutorials:
+## Respources and tutorials:
 
-https://www.youtube.com/watch?v=ngBI40tjirE
 
 1. [Confiuguring AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html)
+2. [S3 demos with boto](https://github.com/boto/boto3/blob/develop/boto3/examples/s3.rst#list-top-level-common-prefixes-in-amazon-s3-bucket)
